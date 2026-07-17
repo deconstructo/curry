15451df79d37b290d6954414b06909d14d3c2b3c
Scáth <metanoia@gmail.com>
2026-07-17 20:05:56 +1000

    chore(formula): sha256 for v1.8.0 tarball

--- Formula/curry.rb
+++ Formula/curry.rb
@@ -7,7 +7,7 @@ class Curry < Formula
   #   git tag v1.2.6 && git push origin v1.2.6
   #   curl -L https://github.com/deconstructo/curry/archive/refs/tags/v1.6.2.tar.gz | shasum -a 256
   url "https://github.com/deconstructo/curry/archive/refs/tags/v1.8.0.tar.gz"
-  sha256 "PLACEHOLDER_UPDATE_AFTER_TAGGING"
+  sha256 "edc32546dbf2cc27d548324f780e3c2fcf33701cf97b7fd3797f63d486c649a5"
   version "1.8.0"
 
   head "https://github.com/deconstructo/curry.git", branch: "main"
