bc488da562bb5b1c4cf6d85373aff8ad9c3b1d64
Scáth <metanoia@gmail.com>
2026-08-22 07:55:02 +1000

    Release v1.22.1

--- Formula/curry.rb
+++ Formula/curry.rb
@@ -4,11 +4,11 @@ class Curry < Formula
   license "GPL-3.0-only"
 
   # Update url + sha256 after tagging a release:
-  #   git tag v1.22.0 && git push origin v1.22.0
-  #   curl -L https://github.com/deconstructo/curry/archive/refs/tags/v1.22.0.tar.gz | shasum -a 256
-  url "https://github.com/deconstructo/curry/archive/refs/tags/v1.22.0.tar.gz"
-  sha256 "bd645fe2f028df3c18d6aff7682b414cde5d4f250ce8db97426dec0bac86e61e"
-  version "1.22.0"
+  #   git tag v1.22.1 && git push origin v1.22.1
+  #   curl -L https://github.com/deconstructo/curry/archive/refs/tags/v1.22.1.tar.gz | shasum -a 256
+  url "https://github.com/deconstructo/curry/archive/refs/tags/v1.22.1.tar.gz"
+  sha256 "0000000000000000000000000000000000000000000000000000000000000"
+  version "1.22.1"
 
   head "https://github.com/deconstructo/curry.git", branch: "main"
 
