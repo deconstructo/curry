d9427c8f1acb5415c2041899da7791d6570a10aa
Scáth <metanoia@gmail.com>
2026-09-13 16:05:01 +1000

    Update Homebrew formula sha256 for v1.24.0

--- Formula/curry.rb
+++ Formula/curry.rb
@@ -7,7 +7,7 @@ class Curry < Formula
   #   git tag v1.24.0 && git push origin v1.24.0
   #   curl -L https://github.com/deconstructo/curry/archive/refs/tags/v1.24.0.tar.gz | shasum -a 256
   url "https://github.com/deconstructo/curry/archive/refs/tags/v1.24.0.tar.gz"
-  sha256 "6c0372cb3d66db4efb68fe12b729dc7b87c2605d9c4ae3192974f38a6c6afa77"
+  sha256 "65f3f84f6f8017b5d1b9358ac40d5b43d28c3ab30b96b5ec7998bfcd499f1d0c"
   version "1.24.0"
 
   head "https://github.com/deconstructo/curry.git", branch: "main"
